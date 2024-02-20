3c8c82ec3589015d166fb45cf0836cd5891ad61b
lolrida <97251836+lolrida@users.noreply.github.com>
2024-02-20 13:17:33 +0000

    aggiunta su github e aggiunta delle select per le varie tabelle

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+  
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "cinema";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  if($conn->connect_error){
+    echo "errore";
+    header("Location: form.html");
+  } else {
+    echo "molto bene";
+  }
+?>
---
+++ select.php
@@ -0,0 +1,33 @@
+<?php 
+
+include 'connection.php';
+$tabella = $_POST["tabella"];
+
+$sql = "SELECT * FROM $tabella";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Intestazione della tabella
+    echo "<table ><tr>";
+    while ($fieldInfo = $result->fetch_field()) {
+        echo "<th>" . $fieldInfo->name . "</th>";
+    }
+    echo "</tr>";
+
+    // Dati della tabella
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        foreach ($row as $value) {
+            echo "<td>" . $value . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Nessun risultato trovato nella tabella 'attori'.";
+}
+echo "<br><br><br>";
+echo "<a href='./form.html'>Ritorna alla home</a>";
+
+
+
